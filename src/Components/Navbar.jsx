f3379c4e9ad54aa072001c7faffbe752d72b3e4c
clint <clintonwuks@gmail.com>
2022-05-24 13:56:33 +0100

    mobile view

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 import { Link } from "react-router-dom";
 import '../navbar.css';
 
 
-function Navbar() {
+const Navbar = ()  => {
+	const [navToggle, setNavToggle] = useState("collapse navbar-collapse")
+
+	const toggleBar = useCallback(() => {
+		if(navToggle === "collapse navbar-collapse") {
+			setNavToggle("")
+		} else setNavToggle("collapse navbar-collapse")
+	}, [navToggle])
+	
     return (
         <div >
 			<nav className="navbar navbar-expand-lg fixed-top">
 				<div className="container-fluid">
 					<a className="navbar-brand text-white " href="/"><i className="fa fa-gamepad mr-2 " style={{ fontSize:'25px' }}></i>Forever Game</a>
-					<button className="navbar-toggler" type="button" data-toggle="#collapse" data-target="#navConts" aria-controls="navConts" aria-expanded="false">
-						<span className="btn btn-primary navbar-toggler-icon">Hey</span>
+					<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navConts" onClick={toggleBar} >
+						<span className="btn btn-primary navbar-toggler-icon"></span>
 					</button>
-					<div className="collapse navbar-collapse" id="navConts">
+					<div className={navToggle} id="navConts">
 						<ul className="navbar-nav ms-auto">
 							<li className="nav-item pl-1">
 								<Link className="nav-link" to="/"><i className="fa fa-home fa-fw mr-1"></i>Home</Link>
